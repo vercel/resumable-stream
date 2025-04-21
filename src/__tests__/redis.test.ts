ed239ec07bd74e77aba2d179f4fa982f618c0dc6
Malte Ubl <malte.ubl@gmail.com>
2025-04-21 08:38:19 -0700

    Run CI

--- src/__tests__/redis.test.ts
+++ src/__tests__/redis.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "vitest";
 import { resumableStreamTests } from "./tests";
 
 if (process.env.REDIS_URL) {
@@ -9,4 +10,9 @@ if (process.env.REDIS_URL) {
   });
 } else {
   console.error("REDIS_URL is not set, skipping tests");
+  describe("Redis tests", () => {
+    it("should be skipped", () => {
+      expect(true).toBe(true);
+    });
+  });
 }
