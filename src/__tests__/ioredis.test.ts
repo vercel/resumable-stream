f05e2b42e123b6e0a9f8af3f3aa640ceec035c0c
Ethan Shea <ethan.shea@vercel.com>
2025-10-15 18:05:02 -0400

    Fix double-connect when using the default clients for ioredis (#34)
    I was getting this error when creating a resumableStreamContext
    
    ```
    Redis is already connecting/connected
    ```
    
    ioredis will connect clients automatically when constructed
    https://github.com/redis/ioredis/blob/main/README.md#basic-usage

--- src/__tests__/ioredis.test.ts
+++ src/__tests__/ioredis.test.ts
@@ -3,12 +3,24 @@ import { resumableStreamTests } from "./tests";
 import Redis from "ioredis";
 
 if (process.env.REDIS_URL) {
-  resumableStreamTests(() => {
-    return {
-      subscriber: new Redis(process.env.REDIS_URL!),
-      publisher: new Redis(process.env.REDIS_URL!),
-    };
-  }, "ioredis");
+  describe("default client", () => {
+    resumableStreamTests(() => {
+      // Will obtain credentials from the REDIS_URL environment variable automatically
+      return {
+          subscriber: undefined,
+          publisher: undefined,
+        };
+      }, "ioredis");
+  });
+
+  describe("custom client", () => {
+    resumableStreamTests(() => {
+      return {
+        subscriber: new Redis(process.env.REDIS_URL!),
+        publisher: new Redis(process.env.REDIS_URL!),
+      };
+    }, "ioredis");
+  });
 } else {
   console.error("REDIS_URL is not set, skipping tests");
   describe("Redis tests", () => {
